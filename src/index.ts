c28ae11c45ed7dbac03067ee8a2374ba98879492
Flavio de Lima <flaviolimpio@gmail.com>
2024-03-05 20:57:45 -0300

    feat: add express server

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,12 @@
-console.log("Try npm run lint/fix!");
+import express from 'express';
 
-const longString = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut aliquet diam.';
+const app = express();
+const port = 3000;
 
-const trailing = 'Semicolon'
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-			const why={am:'I tabbed?'};
-
-const iWish = "I didn't have a trailing space..."; 
-
-const sicilian = true;;
-
-const vizzini = (!!sicilian) ? !!!sicilian : sicilian;
-
-const re = /foo   bar/;
-
-export function doSomeStuff(withThis: string, andThat: string, andThose: string[]) {
-    //function on one line
-    if(!Boolean(andThose.length)) {return false;}
-    console.log(withThis);
-    console.log(andThat);
-    console.dir(andThose);
-    console.log(longString, trailing, why, iWish, vizzini, re);
-    return;
-}
-// TODO: more examples
+app.listen(port, () =>
+  console.log(`Express is listening at http://localhost:${port}`)
+);
